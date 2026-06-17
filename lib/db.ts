4721974a5b447e985ff01e39e71b4a7e58853410
divakar singh purva <divakarpurva04@gmail.com>
2026-06-17 16:51:48 +0530

    feat: add prisma and postgres DB

---
+++ lib/db.ts
@@ -0,0 +1,24 @@
+import {PrismaPg} from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/client";
+
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined;
+};
+
+function CreatePrismaClient(){
+    const url = process.env.DATABASE_URL;
+    if(!url){
+        throw new Error("Database URL not found");
+
+    }
+
+
+    const adapter = new PrismaPg({connectionString: url});
+    return new PrismaClient({adapter});
+}
+
+export const prisma = globalForPrisma.prisma ?? CreatePrismaClient();
+
+if(process.env.NODE_ENV !== "production"){
+    globalForPrisma.prisma = prisma;
+}
